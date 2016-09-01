93e58c7016f49939949e18a92d83c56bacf67fe9
tbobo <tlsong54321@gmail.com>
2016-08-31 23:25:53 -0500

    Initial Commit

---
+++ tbobo/GameOfLife.java
@@ -0,0 +1,14 @@
+package tbobo;
+
+public class GameOfLife {
+	
+	private boolean[][] gameGrid;
+	
+	public GameOfLife() {
+		gameGrid = new boolean[10][10];
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
