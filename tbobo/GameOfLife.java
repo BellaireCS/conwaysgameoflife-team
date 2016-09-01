a070956392e22e62d04cdeaae40c433176be2296
tbobo <tlsong54321@gmail.com>
2016-09-01 00:18:14 -0500

    Added basic initializer and properties

--- tbobo/GameOfLife.java
+++ tbobo/GameOfLife.java
@@ -1,14 +1,52 @@
 package tbobo;
 
+import java.util.Random;
+
 public class GameOfLife {
 	
 	private boolean[][] gameGrid;
+	private int time;
+	private int population;
+	private static final int size;
 	
-	public GameOfLife() {
-		gameGrid = new boolean[10][10];
+	static {
+		size = 10;
 	}
 	
-	public static void main(String[] args) {
+	public GameOfLife() {
+		gameGrid = new boolean[size][size];
+		time = 0;
+		population = 0;
 		
+		Random randomBool = new Random();
+		
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < gameGrid[i].length; j++) {
+				if(gameGrid[i][j] = randomBool.nextBoolean())
+					population++;
+			}
+		}
+	}
+	
+	// Potential console display; swing might be better
+	/*
+	public void display() {	
+		for (int i = 0; i < gameGrid.length; i++) {
+			for (int j = 0; j < gameGrid[i].length; j++) {
+				char cell = (gameGrid[i][j]) ? 'X' : '-';
+				
+				System.out.print(cell + " ");
+			}
+			
+			System.out.println();
+		}
+		
+		System.out.println("time: " + time);
+		System.out.println("population: " + population);
+	}
+	*/
+	
+	public static void main(String[] args) {
+		GameOfLife test = new GameOfLife();
 	}
 }
